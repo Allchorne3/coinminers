390b2bb256e367eb4018498e18a58288314ddc3b
Stefan Allchorne <allchorne39@sky.com>
2023-09-25 18:40:34 +0100

    Final setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,9 +7,7 @@ mix
 
     // JavaScript
     .js('./site/src/scripts/app.js', 'public/scripts').setPublicPath('public')
-    .js('./site/src/scripts/calculator.js', 'public/scripts').setPublicPath('public')
     .js('./site/src/scripts/exportTable.js', 'public/scripts').setPublicPath('public')
-    .js('./site/src/scripts/tabs.js', 'public/scripts').setPublicPath('public')
 
     // CSS
     .sass('./site/src/style/style.scss', 'public/style/style.css')
